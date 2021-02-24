518e5c10f18209c4ae3a769f553e1c3b0b30f8d8
ayoubbouya <bouya.ayoub1@gmail.com>
2021-02-24 20:57:35 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header from "./Header/Header";
+
+function App() {
+  return (
+    <div className="background">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header/Header.js
@@ -0,0 +1,35 @@
+import React from "react";
+import styles from "./styles.module.css";
+import { FaCheckCircle, FaUserAlt } from "react-icons/fa";
+import { HiOutlineDocumentReport } from "react-icons/hi";
+import { FiSettings } from "react-icons/fi";
+
+function Header() {
+  return (
+    <div>
+      <header className={styles.header}>
+        <h2>
+          {" "}
+          <FaCheckCircle /> Pomo{" "}
+        </h2>
+        <ul className={styles.nav}>
+          <li className={styles.list}>
+            {" "}
+            <HiOutlineDocumentReport /> reports
+          </li>
+          <li className={styles.list}>
+            {" "}
+            <FiSettings />
+            settings
+          </li>
+          <li className={styles.list}>
+            <FaUserAlt /> login
+          </li>
+          <h3> </h3>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
