5946127fac5bb219435e731c49fd950760a7e7d6
ayoubbouya <bouya.ayoub1@gmail.com>
2021-02-28 17:58:03 +0200

    updated

--- src/Counter/Counter.js
+++ src/Counter/Counter.js
@@ -1,44 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
+import Countdown from "react-countdown";
+
 import "./styles.css";
 const Counter = (props) => {
-  const { selectedTab, setSelectedTab } = props;
+  //   countdown timer
+  const renderer = ({ formatted: { minutes, seconds, completed } }) => {
+    if (completed) {
+      // Render a completed state
+      return <Completionist />;
+    } else {
+      // Render a countdown
+      return (
+        <span>
+          {minutes}:{seconds}
+        </span>
+      );
+    }
+  };
+  const Completionist = () => <span>You are good to go!</span>;
+
+  // useState for background color of lists
+  const { bgColor, setBgColor } = props;
+  //  useState for numbers "  min "
+  const [min, setMin] = useState("25:00");
 
   const changed1 = () => {
-    setSelectedTab(1);
+    setBgColor(1);
+    setMin(<Countdown date={Date.now() + 1500000} renderer={renderer} />);
   };
 
   const changed2 = () => {
-    setSelectedTab(2);
+    setBgColor(2);
+    setMin(<Countdown date={Date.now() + 300000} renderer={renderer} />);
   };
   const changed3 = () => {
-    setSelectedTab(3);
+    setBgColor(3);
+    setMin(<Countdown date={Date.now() + 900000} renderer={renderer} />);
   };
-
+  // play and pause
+  const [start, setStart] = useState(false);
+  const pause = () => {
+    setStart(!start);
+  };
   return (
     <div>
       <section className="counterSection">
         <ul className="breakList">
           <li
-            className={"list" + " " + (selectedTab === 1 ? "bg1" : null)}
+            className={"list" + (bgColor === "list" ? "bg1" : " ")}
             onClick={changed1}
           >
             pomo
           </li>
           <li
-            className={"list" + " " + (selectedTab === 2 ? "bg2" : "")}
+            className={"list" + (bgColor === "list" ? "bg2" : " ")}
             onClick={changed2}
           >
             Short break
           </li>
           <li
-            className={"list" + " " + (selectedTab === 3 ? "bg3" : "")}
+            className={"list" + (bgColor === "list" ? "bg3" : " ")}
             onClick={changed3}
           >
             long break
           </li>
         </ul>
-        <h1>25:00</h1>
-        <button>start</button>
+        <h1>
+          <h2>{min}</h2>
+          <button>
+            {start ? (
+              <span onClick={pause}>Start</span>
+            ) : (
+              <span onClick={pause}>Pause</span>
+            )}
+          </button>
+        </h1>
       </section>
     </div>
   );
